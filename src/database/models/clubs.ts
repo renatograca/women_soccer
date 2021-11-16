df4f4b1cddbd4aa8d08388335c8ca055489dab40
Renato Graça <renatomacedo1995@gmail.com>
2021-11-16 19:13:51 -0300

    seting associate

--- src/database/models/clubs.ts
+++ src/database/models/clubs.ts
@@ -1,25 +1,9 @@
 import * as Sequelize from 'sequelize';
 import db from '.';
-import Players from './players';
+// import Players from './players';
 
-class Clubs extends Sequelize.Model {
-  /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-  public static associate() {
-    // define association here
-    // Clubs.belongsTo(Players, { foreignKey: 'club_id', as: 'playersclubs' });
-    Clubs.belongsTo(Players, { foreignKey: 'club_id', as: 'playersclubs' });
+class Clubs extends Sequelize.Model {}
 
-    Players.hasMany(Clubs, {
-      sourceKey: 'club_id',
-      foreignKey: 'club_id',
-      as: 'clubsplayers', // this determines the name in `associations`!
-    });
-  }
-}
 Clubs.init({
   club_id: Sequelize.INTEGER,
   club_name: Sequelize.STRING,
@@ -27,14 +11,6 @@ Clubs.init({
   sequelize: db.connection,
   modelName: 'clubs',
   timestamps: false,
-});
-
-Clubs.belongsTo(Players, { foreignKey: 'club_id', as: 'playersclubs' });
-
-Players.hasMany(Clubs, {
-  sourceKey: 'club_id',
-  foreignKey: 'club_id',
-  as: 'clubsplayers', // this determines the name in `associations`!
 });
 
 export default Clubs;
--- src/database/models/players.ts
+++ src/database/models/players.ts
@@ -2,29 +2,8 @@ import * as Sequelize from 'sequelize';
 import db from '.';
 import Clubs from './clubs';
 
-class Players extends Sequelize.Model {
-  /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-  associate() {
-    // define association here
-    // Players.hasMany(Clubs, {
-    //   sourceKey: 'club_id',
-    //   foreignKey: 'club_id',
-    //   as: 'clubsplayers', // this determines the name in `associations`!
-    // });
+class Players extends Sequelize.Model {}
 
-    Players.belongsTo(Clubs, { foreignKey: 'club_id', as: 'playersclubs' });
-
-    Clubs.hasMany(Players, {
-      sourceKey: 'id',
-      foreignKey: 'club_id',
-      as: 'clubsplayers', // this determines the name in `associations`!
-    });
-  }
-}
 Players.init({
   player_id: Sequelize.INTEGER,
   player_name: Sequelize.STRING,
@@ -33,6 +12,13 @@ Players.init({
   sequelize: db.connection,
   modelName: 'players',
   timestamps: false,
+});
+Clubs.belongsTo(Players, { foreignKey: 'club_id', as: 'playersclubs' });
+
+Players.hasMany(Clubs, {
+  sourceKey: 'club_id',
+  foreignKey: 'club_id',
+  as: 'clubsplayers', // this determines the name in `associations`!
 });
 
 export default Players;
