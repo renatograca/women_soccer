15199cdf9fd9414b1d0af079d3e9b80dcdbd1adc
Tiago <tiagaoalb@gmail.com>
2021-11-23 07:33:53 -0300

    cleaned some code

--- src/middlewares/MatchMiddleware.ts
+++ src/middlewares/MatchMiddleware.ts
@@ -1,24 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-// import { verify } from 'jsonwebtoken';
-
-// import MatchService from '../services/MatchesService';
-
-// const secret = '123456';
-
-// export default async (req: Request, res: Response, next: NextFunction) => {
-//   const { id: matchId } = req.params;
-//   const token = req.body.authorization;
-
-//   const { id, role } = verify(token, secret);
-
-//   const { userId } = MatchService.getOneMatch(Number(matchId));
-
-//   if (role !== 'admin' || !id === userId) {
-//     return res.status(401).json('User without permission!');
-//   }
-
-//   return next();
-// };
 
 class MatchMiddleware {
   async validateTeam(req: Request, res: Response, next: NextFunction) {
