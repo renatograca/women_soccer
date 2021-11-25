ef651139b8c33441dae24493f211dce961b3f667
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-11-25 07:30:19 -0300

    Add request to get Leaderboard data

--- frontend/src/components/Leaderboard.jsx
+++ frontend/src/components/Leaderboard.jsx
@@ -1,55 +1,73 @@
-import React from 'react';
-import leaderboard from '../tests/mock/leaderboarMock';
+import React, { useState, useEffect } from 'react';
+import requestLeaderboard from '../services/requests';
+import Loading from './Loading';
 
-const Leaderboard = () => (
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Classificação</th>
-          <th>P</th>
-          <th>J</th>
-          <th>V</th>
-          <th>E</th>
-          <th>D</th>
-          <th>GP</th>
-          <th>GC</th>
-          <th>SG</th>
-          <th>%</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          leaderboard.map(({
-            name,
-            TotalPoints,
-            TotalGames,
-            TotalVictories,
-            TotalDraws,
-            TotalLooses,
-            GoalsFavor,
-            GoalsOwn,
-            GoalsBalance,
-            Efficiency
-          },
-          index) => (
-            <tr>
-              <td>{`${index + 1} ${name}`}</td>
-              <td>{ TotalPoints }</td>
-              <td>{ TotalGames }</td>
-              <td>{ TotalVictories }</td>
-              <td>{ TotalDraws }</td>
-              <td>{ TotalLooses }</td>
-              <td>{ GoalsFavor }</td>
-              <td>{ GoalsOwn }</td>
-              <td>{ GoalsBalance }</td>
-              <td>{ Efficiency }</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
-  </div>
-);
+const Leaderboard = () => {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    const url = 'http://localhost:3001'
+    const endpoint = '/result';
+
+    requestLeaderboard(url, endpoint)
+      .then((response) => setLeaderboard(response))
+      .catch((error) => console.log(error));
+  }, [leaderboard])
+
+  if (leaderboard.length === 0) {
+    return (<Loading />)
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Classificação</th>
+            <th>P</th>
+            <th>J</th>
+            <th>V</th>
+            <th>E</th>
+            <th>D</th>
+            <th>GP</th>
+            <th>GC</th>
+            <th>SG</th>
+            <th>%</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            leaderboard.map(({
+              name,
+              totalPoints,
+              totalGames,
+              totalVictories,
+              totalDraws,
+              totalLooses,
+              goalsFavor,
+              goalsOwn,
+              goalsBalance,
+              efficiency
+            },
+            index) => (
+              <tr>
+                <td>{`${index + 1} ${name}`}</td>
+                <td>{ totalPoints }</td>
+                <td>{ totalGames }</td>
+                <td>{ totalVictories }</td>
+                <td>{ totalDraws }</td>
+                <td>{ totalLooses }</td>
+                <td>{ goalsFavor }</td>
+                <td>{ goalsOwn }</td>
+                <td>{ goalsBalance }</td>
+                <td>{ efficiency }</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+};
 
 export default Leaderboard;
---
+++ frontend/src/services/requests.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const requestLeaderboard = async (url, endpoint) => {
+  const { data } = await axios.get(`${url}${endpoint}`);
+  return data;
+}
+
+export default requestLeaderboard;
