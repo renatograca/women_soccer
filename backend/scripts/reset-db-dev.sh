f5f5603182e4cf9b36e8f2c2e587365641f4cb30
Renato Graça <renatomacedo1995@gmail.com>
2021-12-07 10:34:35 -0300

    movendo backend e frontend para a pasta app

--- backend/scripts/reset-db-dev.sh
+++
@@ -1,4 +0,0 @@
-npx sequelize-cli db:drop
-npx sequelize-cli db:create
-npx sequelize-cli db:migrate
-npx sequelize-cli db:seed:all
--- backend/src/routes/ClubsRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ClubsController from '../controllers/ClubsController';
-
-const router = Router();
-
-router.get('/clubs', ClubsController.getAllClubs);
-router.get('/clubs/:id', ClubsController.getOneClub);
-
-export default router;
