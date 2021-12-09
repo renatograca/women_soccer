b066d40e00aeecdbc35a4f181b65a518b71a9865
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-12-09 09:50:05 -0300

    Atualiza seeder de usuarios

--- app/backend/src/database/seeders/20211205212238-user.js
+++ app/backend/src/database/seeders/20211205212238-user.js
@@ -5,12 +5,20 @@ module.exports = {
      *
      * Example:
      */
-    await queryInterface.bulkInsert('users', [{
-      username: 'renato',
-      role: 'admin',
-      email: 'renato@email.com',
-      password: '$2a$08$LZvXyfNVVWr65SaBueSe.O3mFah/JVPKDChCbScC4vyCcKM2iMe8G',
-    }], {});
+    await queryInterface.bulkInsert('users', [
+      {
+        username: 'Admin',
+        role: 'admin',
+        email: 'admin@admin.com',
+        password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW', // secret_admin
+      },
+      {
+        username: 'User',
+        role: 'user',
+        email: 'user@user.com',
+        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', // secret_user
+      },
+    ], {});
   },
 
   down: async (queryInterface) => {
